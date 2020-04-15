f2de8ef89bc1802ea659f59301b81e1dff3628f1
rober_golf <roberttos_carlos@hotmail.com>
2020-04-15 17:01:34 +0200

    conexion firebase

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -18,7 +18,7 @@ export class AppComponent implements OnInit {
     ) {}
 
     ngOnInit(): void {
-        this.analytics.trackPageViews();
-        this.seoService.trackCanonicalChanges();
+        // this.analytics.trackPageViews();
+        // this.seoService.trackCanonicalChanges();
     }
 }
