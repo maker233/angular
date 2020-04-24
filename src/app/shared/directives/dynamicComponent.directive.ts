65ea0f09a267c270cbebcce3653cd454843abcb4
yvyncl <yvyn85@gmail.com>
2020-04-24 18:41:56 +0200

    Eliminada directiva innecesaria

--- src/app/shared/directives/dynamicComponent.directive.ts
+++
@@ -1,8 +0,0 @@
-import { Directive, ViewContainerRef } from '@angular/core';
-
-@Directive({
-    selector: '[dynamic-component]',
-})
-export class DynamicComponentDirective {
-    constructor(public viewContainerRef: ViewContainerRef) {}
-}
