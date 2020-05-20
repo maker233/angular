358704981062b98566ce48dc3687446bb89d51be
yvyncl <yvyn85@gmail.com>
2020-05-20 19:46:22 +0200

    Finalizada creación de examen

--- src/app/modules/exams/new-exam/components/new-exam-dialog/new-exam-dialog.component.ts
+++ src/app/modules/exams/new-exam/components/new-exam-dialog/new-exam-dialog.component.ts
@@ -1,16 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { NbDialogRef } from '@nebular/theme';
+import { ExamsService } from '../../../../../shared/services/exams.service';
+import { TitulacionBBDDModel } from '../../../../../shared/resources/titulacion-bbdd.model';
+import { NivelesBBDDModel } from '../../../../../shared/resources/niveles-bbdd.model';
 
 @Component({
     selector: 'ngx-new-exam-dialog',
     templateUrl: './new-exam-dialog.component.html',
     styleUrls: ['./new-exam-dialog.component.scss'],
 })
-export class NewExamDialogComponent {
-    selectedTitu: string;
-    selectedNivel: string;
+export class NewExamDialogComponent implements OnInit {
+    selectedTitu: TitulacionBBDDModel;
+    selectedNivel: NivelesBBDDModel;
 
-    constructor(protected ref: NbDialogRef<NewExamDialogComponent>) {}
+    titulaciones: TitulacionBBDDModel[];
+    niveles: NivelesBBDDModel[];
+
+    constructor(
+        protected ref: NbDialogRef<NewExamDialogComponent>,
+        private examsService: ExamsService
+    ) {}
+
+    ngOnInit() {
+        this.examsService.getTitulaciones().subscribe((data) => {
+            this.titulaciones = data;
+        });
+    }
 
     cancel() {
         this.ref.close();
@@ -18,8 +33,29 @@ export class NewExamDialogComponent {
 
     aceptar() {
         this.ref.close({
-            titulo: this.selectedTitu,
-            nivel: this.selectedNivel,
+            titulo: this.selectedTitu.id,
+            nivel: this.selectedNivel.id,
         });
+    }
+
+    tituloSeleccionado() {
+        this.selectedNivel = null;
+        this.examsService
+            .getNivelesTitulacion(this.selectedTitu.id)
+            .subscribe(
+                (data) =>
+                    (this.niveles = data.sort((a, b) =>
+                        this.compareToNiveles(a, b)
+                    ))
+            );
+    }
+
+    compareToNiveles(a: NivelesBBDDModel, b: NivelesBBDDModel) {
+        if (a.orden < b.orden) {
+            return -1;
+        } else if (a.orden > b.orden) {
+            return 1;
+        }
+        return 0;
     }
 }
---
+++ src/app/shared/resources/titulacion-bbdd.model.ts
@@ -0,0 +1,5 @@
+export class TitulacionBBDDModel {
+    id: string;
+    codigo: string;
+    nombre: string;
+}
